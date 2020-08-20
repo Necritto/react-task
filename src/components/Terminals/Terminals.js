d3404b63c5dac9da1374961f0eea8b577868202e
Necritto <onetopyr@mail.ru>
2020-08-20 16:52:59 +0300

    Feature: Terminals component logic [ref:no-ref]

--- src/components/Terminals/Terminals.js
+++ src/components/Terminals/Terminals.js
@@ -1,17 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Terminals.module.scss";
 
 export const Terminals = () => {
+  const [terminal, setTerminal] = useState("");
+  const [description, setDescription] = useState("");
+  const [listTerminals, setListTerminals] = useState([]);
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    if (terminal && description) {
+      const list = {
+        terminal,
+        description,
+      };
+      setListTerminals([...listTerminals, list]);
+      setTerminal("");
+      setDescription("");
+    }
+  };
+
+  const userInputHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    name === "terminal" ? setTerminal(value) : setDescription(value);
+  };
+
+  const onRemoveTerminal = (rowIndex) => {
+    const filteredList = listTerminals.filter((_, index) => index !== rowIndex);
+    setListTerminals(filteredList);
+  };
+
   return (
     <div className={classes.terminals}>
       <h1>Терминалы</h1>
       <div className={classes.container}>
-        <form className={classes.terminals_form}>
+        <form className={classes.terminals_form} onSubmit={onSubmitHandler}>
           <div className={classes.terminals_form__input}>
-            <input type="text" placeholder="Название терминала" name="terminal" />
+            <input
+              type="text"
+              placeholder="Название терминала"
+              name="terminal"
+              value={terminal}
+              onChange={userInputHandler}
+            />
           </div>
           <div className={classes.terminals_form__input}>
-            <textarea placeholder="Описание" name="description" />
+            <textarea
+              placeholder="Описание"
+              name="description"
+              value={description}
+              onChange={userInputHandler}
+            />
           </div>
           <div className={classes.terminals_form__input}>
             <input type="submit" value="Добавить" />
@@ -30,6 +69,16 @@ export const Terminals = () => {
                 <th>Описание</th>
                 <th></th>
               </tr>
+              {listTerminals &&
+                listTerminals.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.terminal}</td>
+                    <td>{item.description}</td>
+                    <td>
+                      <button onClick={() => onRemoveTerminal(index)}>&times;</button>
+                    </td>
+                  </tr>
+                ))}
             </tbody>
           </table>
         </div>
